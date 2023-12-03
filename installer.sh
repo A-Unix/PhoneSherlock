88d32098190721f70c16cde121f64c8f257cda5b
Atul93 <54507991+Atuls-git@users.noreply.github.com>
2023-12-03 10:41:44 +0530

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+echo -e "\nChecking if Pip3 is already installed\n"
+
+# Check if python3-pip is already installed
+if python3 -c "import pkg_resources; pkg_resources.get_distribution('pip')" >/dev/null 2>&1; then
+    echo -e "\nPip3 is already installed.\n"
+else
+    echo -e "\nOops! Pip3 not found on your system, don't worry, I'm installing it for you :)\n"
+    sleep 1
+    echo -e "\nInstalling Pip3 now!\n"
+    sleep 1
+
+    # Install python3-pip using apt
+    sudo apt install python3-pip -y
+    sleep 1
+
+    echo -e "Done!\n"
+fi
+
+# Check if colorama is already installed
+if python3 -c "import pkg_resources; pkg_resources.get_distribution('colorama')" >/dev/null 2>&1; then
+    echo "Colorama is already installed."
+else
+    echo -e "Oops! Colorama not found on your system, don't worry, I'm installing it for you :)\n"
+    sleep 2
+
+    # Install colorama using pip
+    pip install colorama
+
+    echo -e "\nDone! Colorama has been installed successfully.\n"
+fi
+
+# Clear the screen
+clear
+sleep 1
+
+# Check if git is already installed
+if ! command -v git &>/dev/null; then
+    echo "Git is not installed. Installing it now, please wait."
+else
+    # Install git using apt
+    sudo apt install git -y
+
+    echo -e "\nDone! git has been installed successfully.\n"
+fi
+
+repository_url='https://github.com/Atuls-git/kali-anonsurf.git'
+script_folder='kali-anonsurf'
+
+# Check if kali-anonsurf is already installed
+if ! dpkg -s kali-anonsurf &>/dev/null; then
+    echo "Installing kali-anonsurf..."
+    sudo apt-get update
+else
+
+    # Clone the repository
+    echo "Cloning into the repository!"
+    git clone "$repository_url"
+    echo -e "\e[32mDone!\n\e[0m"
+
+    # Move to the cloned folder
+    cd "$script_folder" || exit 1
+
+    # Set necessary permissions
+    chmod +x installer.sh
+
+    # Install the script
+    ./installer.sh
+
+    echo -e "\e[36mRunning the program now, please wait!\n\e[0m"
+
+    # Start the script
+    anonsurf start
+
+    # Give a success message to the user
+    echo -e "\e[32mDone! You're undercover now ;)\n\e[0m"
+
+    cd ..
+fi
+
+
+# Get executive permissions for the main script
+# chmod +x main.py
+
+# Run the main script
+# python3 main.py
---
+++ phonesherlock.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import requests
+import os
+import time
+from colorama import init, Fore
+
+# Initialize colorama
+init()
+
+print("Please wait!")
+
+os.system("clear")
+
+time.sleep(1)
+
+print("\nWelcome!")
+
+time.sleep(2)
+
+banner = r'''
+                                      _ _ _                                        _ _ _                 _ _ _   
+                /\       |\      |  /       \   |\      | \       / |\      /|   /       \   |     |   /       \ 
+               /  \      | \     | |         |  | \     |  \     /  | \    / |  |         |  |     |  |         |
+              /    \     |  \    | |         |  |  \    |   \   /   |  \  /  |  |         |  |     |  |          
+             /_ _ _ \    |   \   | |         |  |   \   |    \ /    |   \/   |  |         |  |     |   \_  _ _   
+            /        \   |    \  | |         |  |    \  |     |     |        |  |         |  |     |           \ 
+           /          \  |     \ | |         |  |     \ |     |     |        |  |         |  |     |  |         |
+          /            \ |      \|  \_ _ _ _/   |      \|     |     |        |   \_ _ _ _/   \_ _ _/  \ _  _  _/ 
+
+|| You may get an error if you do not enter your webpage address correctly. ||
+|| This tool was created by Atul ||
+|| Just so you know, I'm not responsible for the misuse of this tool, it has been created for educational purposes only, and you can use it at your own responsibility! ||
+'''
+print(banner)
+
+time.sleep(7)
+
+import datetime
+now = datetime.datetime.now()
+print("Current date and time:")
+print(now.strftime("%d-%m-%Y %H:%M:%S"))
+
+time.sleep(2)
+
+import sys
+print("Python version:")
+print(sys.version)
+print("Python version:", sys.version_info)
+
+time.sleep(2)
+
+# Check if the script is running as root
+if os.geteuid() != 0:
+    print("This script must be run as root")
+    exit(1)
+
+time.sleep(1)
+
+import subprocess
+import pkg_resources
+
+# Waiting for user response
+input(Fore.GREEN + "Press Enter to continue...")
+
+# Specify the website URL
+website_url = input(Fore.BLUE + "Please enter webpage url Ex: https://example.com >>> ")
+
+# Define a function to check the backend process status
+def check_website_url_status():
+    # Replace this with your actual logic to check the backend process status
+    return False
+
+website_url_running = True
+
+while website_url_running:
+    print(Fore.BLUE + "[+] Finding Numbers", end="")
+
+    # Blinking effect using a loop
+    for _ in range(3):
+        print(".", end="", flush=True)
+        time.sleep(0.5)  # Delay between each dot
+
+    # Clearing the dots using a backspace character (\b)
+    print("\b" * 3, end="", flush=True)
+    time.sleep(0.5)  # Delay between removing the dot
+
+    # Check if the backend process has finished
+    website_url_running = check_website_url_status()
+
+# Send a GET request to the website
+response = requests.get(website_url)
+
+# Extract phone numbers using regular expressions
+phone_numbers = re.findall(r"\b\d{3}-\d{3}-\d{4}\b", response.text)
+
+# Convert phone numbers to XXXXXXXXXX format
+formatted_numbers = [re.sub(r"\D", "", number) for number in phone_numbers]
+
+# The backend process has finished
+print(Fore.CYAN + "[+] Finding Numbers... Done!\n")
+
+time.sleep(1)
+
+# Print the formatted phone numbers
+if formatted_numbers:
+    print(Fore.GREEN + "\nIndian numbers found:\n")
+    for number in formatted_numbers:
+        print(Fore.YELLOW + number)
+else:
+    print(Fore.RED + "\nNo numbers found.\n")
+
+time.sleep(1)
+
+# Print the other numbers
+if phone_numbers:
+    print(Fore.BLUE + "\nOther numbers found:\n")
+    for number in phone_numbers:
+        print(Fore.MAGENTA + number)
+else:
+    print(Fore.RED + "\nNo other numbers found.")
